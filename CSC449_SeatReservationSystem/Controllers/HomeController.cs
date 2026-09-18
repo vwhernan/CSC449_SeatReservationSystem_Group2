370fbda3cad230dc5fd8a33d1fd4976f4293027b
vwhernan <vinceh729@gmail.com>
2026-09-18 15:03:08 -0700

    Add project files.

---
+++ CSC449_SeatReservationSystem/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using CSC449_SeatReservationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CSC449_SeatReservationSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSC449_SeatReservationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSC449_SeatReservationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
